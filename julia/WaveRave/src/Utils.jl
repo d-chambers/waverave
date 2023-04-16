12dc295488bb3e5c7d5395127342863ea991d275
derrick chambers <djachambeador@gmail.com>
2023-04-15 18:25:53 -0600

    work on domain decomp

--- julia/WaveRave/src/Utils.jl
+++ julia/WaveRave/src/Utils.jl
@@ -24,24 +24,33 @@ end
 Should work for any number of dimensions, but gets very expesnive for
 dimensions above 3.
 """
-function find_division_shape(divsions:: Int, domain_shape:: Tuple)
+function find_division_shape(divisions:: Int, domain_shape:: Tuple)
+    
     shape_array = collect(domain_shape)
+
+    # bail out if one division is requested
+    if divisions == 1
+        return ones(Int, length(shape_array)), shape_array
+    end 
+    
     # first get all possible shapes based on permutations that still have
     # a total of divisions
-    perms = permutations(collect(1:1:divsions), length(domain_shape))
-    sub_1 = filter(x -> prod(x) == divsions, reduce(hcat, collect(perms)))
+    ndims = length(domain_shape)
+    perms = [digits(x - 1, base=divisions, pad=ndims) .+ 1 for x in 1:divisions^ndims]
+    reduced_perms = reduce(hcat, collect(perms))'
+    sub_1 = reduced_perms[prod(reduced_perms, dims=2) .== divisions]
     sub_1_2d = atleast2d(sub_1)
     # next, eliminat any solutuions that don't have a int number of elements
     sub_2 = filter(x -> all((shape_array .% x) .== 0), sub_1_2d)
     sub_2_2d = atleast2d(sub_2)
     if length(sub_2) < 1
-        msg = "$domain shape cannot be divided into $divisions divisions"
+        msg = "$domain_shape cannot be divided into $divisions divisions"
         error(msg)
     end
     # now return the value that maximizes block volume (minimizes ghost zones)
     (_, argmax) = findmax(prod(sub_2_2d, dims=2))
-    best_divisor = sub_2_2d[argmax]
-    return shape_array .÷ best_divisor
+    best_divisor = sub_2_2d[argmax[1], :]
+    return (best_divisor, shape_array .÷ best_divisor)
     
 end
 
--- julia/WaveRave/test/test_utils.jl
+++ julia/WaveRave/test/test_utils.jl
@@ -7,9 +7,23 @@ using Debugger
 
 using WaveRave: find_division_shape
 
-@testset "test divisor shape" begin
-    @testset "test 1D" begin
-        out = find_division_shape(10, (100,))
-        @test out == [10]
-    end
+
+@testset "test 1D" begin
+    # happy paths
+    @test find_division_shape(10, (100,)) == ([10], [10])
+    @test find_division_shape(1, (100,)) == ([1], [100])
+    # ensure error thrown
+    @test_throws "cannot be divided" find_division_shape(11, (100,))
+    # ensure 1 works
+
+end
+
+@testset "test 2D" begin
+    # happy paths
+    @enter find_division_shape(4, (100,100))
+    @enter find_division_shape(4, (100,100)) == ([2, 2], [50, 50])
+    # @test find_division_shape(2, (100,100)) == [100, 50]
+    # @test find_division_shape(6, (300,200)) == [100, 100]
+    # # ensure error thrown
+    # @test_throws "cannot be divided" find_division_shape(11, (100,))
 end
