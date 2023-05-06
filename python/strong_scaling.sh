442b2871f785d97692d3172f33a237c09e917491
ayon8181 <ayon.8181@gmail.com>
2023-05-06 15:45:58 -0600

    scaling exps with for loop

--- python/strong_scaling.sh
+++ python/strong_scaling.sh
@@ -16,27 +16,9 @@ conda activate mpirun
 
 
 #Run WeakScaling Tests
-mpirun -n 1 python src/mesher.py 480 800
-mpirun -n 1 python src/run.py 480 800 5
-mpirun -n 2 python src/mesher.py 480 800
-mpirun -n 2 python src/run.py 480 800 5
-mpirun -n 3 python src/mesher.py 480 800
-mpirun -n 3 python src/run.py 480 800 5
-mpirun -n 4 python src/mesher.py 480 800
-mpirun -n 4 python src/run.py 480 800 5
-mpirun -n 5 python src/mesher.py 480 800
-mpirun -n 5 python src/run.py 480 800 5
-mpirun -n 6 python src/mesher.py 480 800
-mpirun -n 6 python src/run.py 480 800 5
-mpirun -n 7 python src/mesher.py 480 800
-mpirun -n 7 python src/run.py 480 800 5
-mpirun -n 8 python src/mesher.py 480 800
-mpirun -n 8 python src/run.py 480 800 5
-mpirun -n 9 python src/mesher.py 480 800 
-mpirun -n 9 python src/run.py 480 800 5
-mpirun -n 10 python src/mesher.py 480 800 
-mpirun -n 10 python src/run.py 480 800 5
-mpirun -n 11 python src/mesher.py 480 800 
-mpirun -n 11 python src/run.py 480 800 5
-mpirun -n 12 python src/mesher.py 480 800
-mpirun -n 12 python src/run.py 480 800 5
+
+for i in {1..12}
+do
+mpirun -n $i python src/mesher.py 480 800
+mpirun -n $i python src/run.py 480 800 5
+done
--- python/weak_scaling.sh
+++ python/weak_scaling.sh
@@ -16,27 +16,8 @@ conda activate mpirun
 
 
 #Run WeakScaling Tests
-mpirun -n 1 python src/mesher.py 60 100
-mpirun -n 1 python src/run.py 60 100 5
-mpirun -n 2 python src/mesher.py 60 200
-mpirun -n 2 python src/run.py 60 200 5
-mpirun -n 3 python src/mesher.py 60 300
-mpirun -n 3 python src/run.py 60 300 5
-mpirun -n 4 python src/mesher.py 60 400
-mpirun -n 4 python src/run.py 60 400 5
-mpirun -n 5 python src/mesher.py 60 500
-mpirun -n 5 python src/run.py 60 500 5
-mpirun -n 6 python src/mesher.py 60 600
-mpirun -n 6 python src/run.py 60 600 5
-mpirun -n 7 python src/mesher.py 60 700
-mpirun -n 7 python src/run.py 60 700 5
-mpirun -n 8 python src/mesher.py 60 800
-mpirun -n 8 python src/run.py 60 800 5
-mpirun -n 9 python src/mesher.py 60 900 
-mpirun -n 9 python src/run.py 60 900 5
-mpirun -n 10 python src/mesher.py 60 1000 
-mpirun -n 10 python src/run.py 60 1000 5
-mpirun -n 11 python src/mesher.py 60 1100 
-mpirun -n 11 python src/run.py 60 1100 5
-mpirun -n 12 python src/mesher.py 60 1200
-mpirun -n 12 python src/run.py 60 1200 5
+for i in {1..12}
+do
+mpirun -n $i python src/mesher.py 60 100
+mpirun -n $i python src/run.py 60 100*$i 5
+done
