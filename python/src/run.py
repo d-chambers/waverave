0f32b0df861ce7736204c0a708a945bd8a5190fe
ayon8181 <ayon.8181@gmail.com>
2023-05-06 09:57:09 -0600

    modified run scripts

--- python/src/run.py
+++ python/src/run.py
@@ -67,42 +67,46 @@ def time_simulation_with_checkpoint(start, stop, wavefield, rank_source, isx, is
 
 
 
-def solve_2d_mpi(nx, ny, nt, sx, sy, dx, dy, dt, pad, type, checkpoint, output_dir):
+def solve_2d_mpi(nx, ny, nt, sx, sy, dx, dy, dt, pad, type, checkpoint, output_dir, nSims):
+    time_taken=0
+    for j in range(nSims):
     
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-    size = comm.Get_size()
-    if type == 0:
-       source = solver.Ricker(ricker_sigma, ricker_shift, nt, dt)
-       F = source.source_time()
-    else:
-         print("No other source implemented other than Ricker")
-         source = solver.Ricker(nt, dt)
-         F = source.source_time()
+        comm = MPI.COMM_WORLD
+        rank = comm.Get_rank()
+        size = comm.Get_size()
+        if type == 0:
+            source = solver.Ricker(ricker_sigma, ricker_shift, nt, dt)
+            F = source.source_time()
+        else:
+            print("No other source implemented other than Ricker")
+            source = solver.Ricker(nt, dt)
+            F = source.source_time()
 
 
-    par2d = simulation.simul_par(nx, ny , dx, dy, comm, rank, size)
-    par2d.size_allocate()
-    rank_source, isx, isy = par2d.source(sx, sy, pad)
-    
-    
+        par2d = simulation.simul_par(nx, ny , dx, dy, comm, rank, size)
+        par2d.size_allocate()
+        rank_source, isx, isy = par2d.source(sx, sy, pad)
+        
+        
 
-    file = os.path.dirname(os.path.dirname(__file__))+'/outputs/vel_'+str(rank).zfill(5)+'.npy'
+        file = os.path.dirname(os.path.dirname(__file__))+'/outputs/vel_'+str(rank).zfill(5)+'.npy'
 
-    wavefield = solver.wavefield(par2d.lnx, par2d.lny, pad, dx, dy, dt, comm, rank, size, par2d.method)
-    wavefield.vel(file)
-    wavefield.set_exchange()
+        wavefield = solver.wavefield(par2d.lnx, par2d.lny, pad, dx, dy, dt, comm, rank, size, par2d.method)
+        wavefield.vel(file)
+        wavefield.set_exchange()
 
-    start=time.time()
-    if checkpoint == 0:
-       time_simulation(0, nt, wavefield, rank_source, isx, isy, F ,  dt, output_dir, comm) 
-       
-    
-    else:
-         time_simulation_with_checkpoint(0, nt, wavefield, rank_source, isx, isy, F, dt, checkpoint, output_dir, comm)
-    if rank==0:
-       end=time.time()
-       print("Average Time Taken for a "+str(nx)+"x"+str(ny)+" Simulation in "+str(size)+" Processors is "+str((end-start)/nt)+" seconds")
+        start=time.time()
+        if checkpoint == 0:
+            time_simulation(0, nt, wavefield, rank_source, isx, isy, F ,  dt, output_dir, comm) 
+        
+        
+        else:
+            time_simulation_with_checkpoint(0, nt, wavefield, rank_source, isx, isy, F, dt, checkpoint, output_dir, comm)
+        if rank==0:
+            end=time.time()
+            time_taken+=end-start
+            if j == nSims-1:
+               print("Ran "+str(j)+"Simulations: Average Time Taken for a "+str(nx)+"x"+str(ny)+" Simulation in "+str(size)+" Processors is "+str((end-start)/j)+" seconds")
 
  
 
